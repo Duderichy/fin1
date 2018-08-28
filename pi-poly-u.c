09b3f74c2843aae83b51d78f8d054503a79a54c0
duderichy <duderichy@gmail.com>
2018-08-28 15:12:01 -0400

    project finished

--- pi-poly-u.c
+++ pi-poly-u.c
@@ -15,14 +15,15 @@ int main() {
 
     i = 0;
     n = 4;
-    s = 1./sqrt(2.);
-    p = n*s;
+    s = sqrt(2.);
+    p = n / s;
     printf("%3d   %10ld   %.14f    % .14f    % .14f\n", i, n, s, p, M_PI-p);
     
     for(i = 1; i < N; i++) {
-        s = sqrt((1. - sqrt(1. - s*s))/2.);
+        // s = sqrt((1. - sqrt(1. - s*s))/2.);
+	s = sqrt(2 * s * s + 2 * sqrt( s * s * s * s - s * s ));
         n *= 2;
-        p = n*s;
+        p = n / s;
         printf("%3d   %10ld   %.14f    % .14f    % .14f\n", i, n, s, p, M_PI-p);
     }
 
